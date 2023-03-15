7da88d9997251a20326af2b43db85e3cf0c87360
koras <kos6476@yandex.ru>
2023-03-15 09:35:19 +0300

    zxc

--- next.config.js
+++ next.config.js
@@ -1,47 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: true,
-  // future: {
-  //   webpack5: true,
-  // },
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   config.plugins.push(
-  //     new CopyPlugin({
-  //       patterns: [
-  //         {
-  //        //   from: path.join(__dirname, "node_modules/tinymce"),
-  //        //   to: path.join(__dirname, "public/assets/libs/tinymce"),
-  //         },
-  //       ],
-  //     })
-  //   );
-  //   return config;
-  // },
-  // webpackDevMiddleware: (config) => {
-  //   return config;
-  // },
-  webpack: (
-    config,
-  { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-) => {
-  config.plugins = [...config.plugins,
-    new webpack.DefinePlugin({
-      '__REACT_DEVTOOLS_GLOBAL_HOOK__': '({ isDisabled: true })'
-    })
-  ]
-  // Important: return the modified config
-  return config
-},
-  env: {
-    GREETING: process.env.GREETING,
-    API: "test",
-     },
 }
-// module.exports = {
-//   env: {
-//     GREETING: process.env.GREETING,
-//   },
-// };
-// module.exports = nextConfig
- 
+
+module.exports = nextConfig
